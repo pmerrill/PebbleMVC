4ba750b832cfa81e4b14eea2e3fb5df9fa36c53c
pmerrill <petejmerrill@gmail.com>
2021-03-14 15:53:27 -0500

    Some documentation

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
 <h1><?php echo $data['title']; ?></h1>
+<p>This is the PebbleMVC PHP framework. Please refer to the <a href="https://github.com/pmerrill/PebbleMVC" target="_blank">documentation on GitHub</a> for more information.</p>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
